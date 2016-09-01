8ccaf42c555d0f408ab37cd94597456173cd358d
tungtd <tung7749@gmail.com>
2016-09-01 20:37:58 +0700

    fix tlmn logic

---
+++ example/js/logo.js
@@ -0,0 +1,29 @@
+(function () {
+    var wrapper = $('.twist .logo');
+
+    var firstT = wrapper.find('.first-t');
+
+    var wCharacter = wrapper.find('.w-character');
+
+    var iCharacter = wrapper.find('.i-character');
+
+    var sCharacter = wrapper.find('.s-character');
+
+    var seconT = wrapper.find('.second-t');
+
+    function initFirstT() {
+        firstT.hightLight = function () {
+            this.find('');
+        };
+        firstT.top = firstT.find('.top');
+        firstT.top = firstT.find('.left');
+        firstT.top = firstT.find('.right');
+        firstT.top = firstT.find('.bottom');
+    }
+    
+    initFirstT();
+    
+    firstT.hightLight();
+
+
+})();
