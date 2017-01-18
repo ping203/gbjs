dd99b0626f1c43d24e365a1085f8d15b66a091d5
tungtd <tung7749@gmail.com>
2017-01-19 04:43:51 +0700

    update gameplay demla

--- src/game/BaseDemlaGame.js
+++ src/game/BaseDemlaGame.js
@@ -34,6 +34,7 @@ this.TWIST = this.TWIST || {};
   };
 
   p.drawPlayingState = function (data) {
+    this._STATUS_PLAYING();
     var players = data.players || [];
     var _self = this;
 
@@ -82,6 +83,7 @@ this.TWIST = this.TWIST || {};
   };
 
   p.dealCards = function (data) {
+    this._STATUS_PLAYING();
     var cardList = data.cardList;
     var players = this.model.players;
     var numberPlayer = 0;
@@ -118,6 +120,12 @@ this.TWIST = this.TWIST || {};
 
     });
 
+  };
+
+  p.STATUS_PLAYING = function () {};
+
+  p._STATUS_PLAYING = function () {
+    TWIST.InRoomGame.prototype.STATUS_PLAYING.call(this);
   };
 
   p.onHitTurn = function (data) {
